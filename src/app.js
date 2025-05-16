d1fa2b0a18f2984832e6928d5b60a8d637e0d883
GuillermoMoralesS <guillermorales2000@gmail.com>
2025-05-16 11:22:48 +0000

    CambiosCartas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+CartaNueva();
 };
+
+function CartaNueva() {
+  let Palos = ["♦", "♥", "♠", "♣"];
+  let Numeros = ["A", "2", "3","4","5","6","7","8","9","10","J","Q","K"];
+
+  let PalosAleatorios= Math.floor(Math.random()*Palos.length);
+  let NumerosAleatorios= Math.floor(Math.random()*Numeros.length);
+
+  let paloSeleccionado = Palos[PalosAleatorios];
+  let numeroSeleccionado = Numeros[NumerosAleatorios];
+
+  
+  let top = document.getElementById("top");
+  let center = document.getElementById("center");
+  let end = document.getElementById("end");
+
+  top.innerText = paloSeleccionado;
+  center.innerText = numeroSeleccionado;
+  end.innerText = paloSeleccionado;
+}
